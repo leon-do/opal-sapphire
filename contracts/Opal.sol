6a76f7c7872e24a697cb5802354e298ed99f3b4c
leon-do <leondo123@gmail.com>
2022-10-02 00:14:11 -0400

    add opal

---
+++ contracts/Opal.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Opal {
+    address private owner;
+
+    mapping(bytes32 => uint256) private balances;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function hashPreimage(uint256 _preimage) public pure returns (bytes32) {
+        return keccak256(abi.encodePacked(_preimage));
+    }
+
+    function deposit(bytes32 _hash) public payable {
+        require(msg.value > 0, "Must deposit value greater than zero");
+        balances[_hash] = msg.value;
+    }
+
+    function withdraw(uint256 _preimage, address _to) public payable {
+        // get balance from preimage
+        uint256 balance = balances[keccak256(abi.encodePacked(_preimage))];
+        require(balance > 0, "Balance is zero");
+        // calculate vaue to send
+        uint256 value = (balance * 9995) / 10000;
+        // calculate fee to send
+        uint256 fee = (balance * 5) / 10000;
+        // zero balance
+        balances[keccak256(abi.encodePacked(_preimage))] = 0;
+        // Send value to account
+        payable(_to).transfer(value);
+        // Send fee to owner
+        payable(owner).transfer(fee);
+    }
+}
